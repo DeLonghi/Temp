fd5e58d8354adc0d8250625eecbd4687e952e6be
Ilya <vinnikov_i@mail.ru>
2020-06-22 02:13:46 +0300

    Added setting panels

--- src/App.js
+++ src/App.js
@@ -7,20 +7,28 @@ import {
   Grid,
   Avatar,
   Typography,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary
 } from "@material-ui/core";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     // display: "flex",
     maxWidth: 1000,
+    maxHeight: 1500,
+    background: "grey",
+    paddingBotton: 35
+
   },
   container: {
     marginBottom: "auto",
     width: "auto",
   },
   paper: {
-    padding: theme.spacing(1),
-    margin: "auto",
+    // padding: theme.spacing(1),
+    marginBottom: 15,
   },
   avatar: {
     width: 100,
@@ -31,11 +39,27 @@ const useStyles = makeStyles((theme) => ({
   },
   TextField: {
     marginTop: 15,
+    marginBottom: 30
+  },
+  table: {
+    // marginBottom: 15
+  },
+  void: {
+    height: 100
   }
-}));
+
+
+}
+));
+
 
 export default function App() {
   const classes = useStyles();
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+  const [expanded, setExpanded] = React.useState(false);
 
   return (
     <Container className={classes.root}>
@@ -92,7 +116,80 @@ export default function App() {
             </Grid>
           </Grid>
         </Grid>
+        <ExpansionPanel style={{}} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+          <ExpansionPanelSummary
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+
+          >
+            <Typography className={classes.heading}>General settings</Typography>
+            <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+              maximus est, id dignissim quam.
+</Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+          <ExpansionPanelSummary
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
+          >
+            <Typography className={classes.heading}>Users</Typography>
+            <Typography className={classes.secondaryHeading}>
+              You are currently not an owner
+</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
+              diam eros in elit. Pellentesque convallis laoreet laoreet.
+</Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        </Paper>
+          <Paper className={classes.paper}>
+        <ExpansionPanel style={{}} expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+          <ExpansionPanelSummary
+            aria-controls="panel3bh-content"
+            id="panel3bh-header"
+          >
+            <Typography className={classes.heading}>Advanced settings</Typography>
+            <Typography className={classes.secondaryHeading}>
+              Filtering has been entirely disabled for whole web server
+</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+              vitae egestas augue. Duis vel est augue.
+</Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        </Paper>
+          <Paper className={classes.paper}>
+        <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+          <ExpansionPanelSummary
+            aria-controls="panel4bh-content"
+            id="panel4bh-header"
+          >
+            <Typography className={classes.heading}>Personal data</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+              vitae egestas augue. Duis vel est augue.
+</Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </Paper>
+      <Paper>
+        <div className={classes.void}>
+
+        </div>
+        </Paper>
     </Container>
   );
 }
